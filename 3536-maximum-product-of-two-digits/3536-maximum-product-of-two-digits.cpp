03a38e5238d806635f65210ab341d28f54facab6
Shivam Kumar Kunwar <kunwarshivam1971@outlook.com>
2026-07-25 12:39:02 +0530

    Time: 0 ms (100%), Space: 9.3 MB (10.11%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        string s = to_string(n);
+        sort(s.begin(), s.end());
+        int len = s.length();
+        
+        // Multiply the two largest digits
+        return (s[len - 1] - '0') * (s[len - 2] - '0');
+    }
+};
