1139788ee3aaef8f4625024a57f86d792ab7aaf3
Shivam Kumar Kunwar <kunwarshivam1971@outlook.com>
2026-08-05 15:20:17 +0530

    Time: 155 ms (91.38%), Space: 301.3 MB (80%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        vector<vector<int>> adj(n);
+        
+        for (auto &e : invocations) {
+            adj[e[0]].push_back(e[1]);
+        }
+
+        // Find all suspicious methods reachable from k
+        vector<int> suspicious(n, 0);
+        queue<int> q;
+        q.push(k);
+        suspicious[k] = 1;
+
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+
+            for (int v : adj[u]) {
+                if (!suspicious[v]) {
+                    suspicious[v] = 1;
+                    q.push(v);
+                }
+            }
+        }
+
+        // If any non-suspicious method invokes a suspicious method,
+        // removal is impossible.
+        for (auto &e : invocations) {
+            int u = e[0], v = e[1];
+            if (!suspicious[u] && suspicious[v]) {
+                vector<int> ans;
+                for (int i = 0; i < n; i++)
+                    ans.push_back(i);
+                return ans;
+            }
+        }
+
+        // Return remaining methods
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            if (!suspicious[i])
+                ans.push_back(i);
+        }
+
+        return ans;
+    }
+};
